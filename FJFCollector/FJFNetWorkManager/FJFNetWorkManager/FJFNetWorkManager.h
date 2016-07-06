f46a6077a8f2b27f28d0c26f6f11ec9061237255
firefly-insky <sonasona@sonasonademini.lan>
2016-07-06 11:08:23 +0800

    添加网路请求封装 FJFNetWorkManager

---
+++ FJFCollector/FJFNetWorkManager/FJFNetWorkManager/FJFNetWorkManager.h
@@ -0,0 +1,16 @@
+//
+//  FJFNetWorkManager.h
+//  FJFNetWorkManager
+//
+//  Created by FJF on 16/7/6.
+//  Copyright © 2016年 FJF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Singleton.h"
+
+@interface FJFNetWorkManager : NSObject
+single_interface(FJFNetWorkManager)
+
+
+@end
---
+++ FJFCollector/FJFNetWorkManager/FJFNetWorkManager/FJFNetWorkManager.m
@@ -0,0 +1,17 @@
+//
+//  FJFNetWorkManager.m
+//  FJFNetWorkManager
+//
+//  Created by FJF on 16/7/6.
+//  Copyright © 2016年 FJF. All rights reserved.
+//
+
+#import "FJFNetWorkManager.h"
+
+@implementation FJFNetWorkManager
+
+single_implementation(FJFNetWorkManager)
+
+
+
+@end
